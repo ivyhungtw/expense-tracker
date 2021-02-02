a73c996b1b98c67c356157327c45accd483e3e94
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-02-02 16:37:09 +0800

    feat: add totoal amount on index.hbs

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -9,7 +9,13 @@ const Record = require('../../models/record')
 router.get('/', (req, res) => {
   Record.find()
     .lean()
-    .then(records => res.render('index', { records }))
+    .then(records => {
+      let totalAmount = 0
+      records.forEach(record => {
+        totalAmount += record.amount
+      })
+      res.render('index', { records, totalAmount })
+    })
     .catch(error => console.log(error))
 })
 
