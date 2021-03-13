bb1d768879a3515419d213ddb98c27fbd47e97ef
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-03-13 11:40:49 -0800

    feat: add user model

---
+++ models/user.js
@@ -0,0 +1,25 @@
+// Require mongoose
+const mongoose = require('mongoose')
+
+// Define Schema
+const Schema = mongoose.Schema
+const userSchema = new Schema({
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+})
+
+// Create and export a model
+module.exports = mongoose.model('User', userSchema)
