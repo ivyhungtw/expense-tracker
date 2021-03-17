3ea180c04f070fa8b3d3e56fd2cd533c121e08b3
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-03-17 16:27:04 -0700

    refactor: modify try/catch statement

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -8,41 +8,38 @@ const Category = require('../../models/category')
 
 // Set up routes of homepage
 router.get('/', async (req, res) => {
-  const userId = req.user._id
-  const dateSet = new Set()
-  let totalAmount = 0
-  let categories
-  let records
-
-  // Find all categories to render category filter
   try {
-    categories = await Category.find().lean().exec()
-  } catch (err) {
-    console.warn(err)
-  }
-
-  try {
+    const userId = req.user._id
+    const dateSet = new Set()
+    let totalAmount = 0
+    // Find all categories to render category filter
+    const categories = await Category.find().lean().exec()
     // Find all records of the user to render record list
-    records = await Record.find({ userId }).lean().sort({ date: 'desc' }).exec()
+    const records = await Record.find({ userId })
+      .lean()
+      .sort({ date: 'desc' })
+      .exec()
 
+    // Iterate over records
     records.forEach(record => {
       // Calculate total amount
       totalAmount += record.amount
       // Store month of the record to dateSet to render month filter
       dateSet.add(record.date.slice(0, 7))
     })
+
     // Format total amount
     totalAmount = new Intl.NumberFormat().format(totalAmount)
+
+    return res.render('index', {
+      records,
+      totalAmount,
+      categoryList: categories,
+      dateSet,
+    })
   } catch (err) {
     console.warn(err)
   }
-
-  return res.render('index', {
-    records,
-    totalAmount,
-    categoryList: categories,
-    dateSet,
-  })
 })
 
 // Export
--- routes/modules/records.js
+++ routes/modules/records.js
@@ -9,88 +9,73 @@ const Category = require('../../models/category')
 // Set up routes
 // Add expense page
 router.get('/new', async (req, res) => {
-  let categories
-
-  // Get category data for category dropdown
   try {
-    categories = await Category.find().lean().exec()
+    // Get category data for category dropdown
+    const categories = await Category.find().lean().exec()
+
+    return res.render('new', { categoryList: categories })
   } catch (err) {
     console.warn(err)
   }
-
-  return res.render('new', { categoryList: categories })
 })
 
 // Confirm creation
 router.post('/', async (req, res) => {
-  const userId = req.user._id
-  // Get form data form request body
-  const record = req.body
-
   try {
+    const userId = req.user._id
+    // Get form data form request body
+    const record = req.body
+    // Find category of the record
     const category = await Category.findOne({ name: record.category })
       .lean()
       .exec()
 
+    // Reassign the record's attribute
     record.date = record.date || Date.now()
     record.amount = parseFloat(record.amount)
     record.categoryIcon = category.icon
     record.userId = userId
-  } catch (err) {
-    console.warn(err)
-  }
 
-  try {
+    // Save the record to Record collection
     const createRecord = await Record.create(record)
+
+    return res.redirect('/')
   } catch (err) {
     console.warn(err)
   }
-
-  return res.redirect('/')
 })
 
 // Edit page
 router.get('/:id/edit', async (req, res) => {
-  // Get user id and expense id
-  const userId = req.user._id
-  const _id = req.params.id
-  let record
-  let categories
-
-  // Find the record by _id and userId
   try {
-    record = await Record.findOne({ _id, userId }).lean().exec()
-  } catch (err) {
-    console.warn(err)
-  }
-
-  // Get category data
-  try {
-    categories = await Category.find().lean().exec()
+    const userId = req.user._id
+    const _id = req.params.id
+    // Find the record by _id and userId
+    const record = await Record.findOne({ _id, userId }).lean().exec()
+    // Get category data
+    const categories = await Category.find().lean().exec()
 
     // Save category status of the record for eq function
     categories.forEach(category => {
       category.tempCategory = record.category
     })
+
+    return res.render('edit', { record, categoryList: categories })
   } catch (err) {
     console.warn(err)
   }
-
-  return res.render('edit', { record, categoryList: categories })
 })
 
 // Confirm editing
 router.put('/:id', async (req, res) => {
-  // Get user id and record _id
-  const userId = req.user._id
-  const _id = req.params.id
-  // Get form data form request body
-  const newRecord = req.body
-
-  let record
-
-  // Find the icon info from category collection
   try {
+    // Get user id and record _id
+    const userId = req.user._id
+    const _id = req.params.id
+    // Get form data form request body
+    const newRecord = req.body
+
+    // Find the icon info from category collection
     const category = await Category.findOne({ name: newRecord.category })
       .lean()
       .exec()
@@ -98,114 +83,92 @@ router.put('/:id', async (req, res) => {
     newRecord.categoryIcon = category.icon
     // Change amount type from string to number
     newRecord.amount = parseFloat(newRecord.amount)
-  } catch (err) {
-    console.warn(err)
-  }
 
-  // Find original record data by _id and userId
-  try {
-    record = await Record.findOne({ _id, userId }).exec()
+    // Find original record data by _id and userId
+    let record = await Record.findOne({ _id, userId }).exec()
     // Reassign new record data and save to record collection
     record = Object.assign(record, newRecord)
-  } catch (err) {
-    console.warn(err)
-  }
-
-  try {
     const saveRecord = await record.save()
+
+    return res.redirect('/')
   } catch (err) {
     console.warn(err)
   }
-
-  // Return to home page
-  return res.redirect('/')
 })
 
 router.delete('/:id', async (req, res) => {
-  const userId = req.user._id
-  const _id = req.params.id
-
   try {
+    const userId = req.user._id
+    const _id = req.params.id
     // Find the record
     const record = await Record.findOne({ _id, userId }).exec()
     // Remove the record from database
-    record.remove()
+    const removeRecord = await record.remove()
+
+    return res.redirect('/')
   } catch (err) {
     console.warn(err)
   }
-
-  // Return to home page
-  return res.redirect('/')
 })
 
 // Filter
 router.get('/', async (req, res) => {
-  const userId = req.user._id
-  const category = req.query.category
-  const date = req.query.date
-  // Initiate filter variable
-  const filter = { userId }
-
-  // If no query string, return home page
-  if (!category && !date) return res.redirect('/')
-  // Add query string to filter
-  if (category) filter.category = category
-  if (date) filter.date = new RegExp('^' + date)
-
-  const dateSet = new Set()
-  let categories
-  let filteredRecords
-  let totalAmount = 0
-
   try {
+    const userId = req.user._id
+    const category = req.query.category
+    const date = req.query.date
+    const dateSet = new Set()
+
+    // Initiate filter variable
+    const filter = { userId }
+    // If there is no query string, return to home page
+    if (!category && !date) return res.redirect('/')
+    // Add query string to filter
+    if (category) filter.category = category
+    if (date) filter.date = new RegExp('^' + date)
+
     // Find all records of the user
     const records = await Record.find({ userId })
       .lean()
       .sort({ date: 'desc' })
       .exec()
-
-    // Store all months of records to dateSet
+    // Iterate over records
+    // Store all months of records to dateSet, except the selected date
     records.forEach(record => {
       const recordDate = record.date.slice(0, 7)
       if (recordDate !== date) dateSet.add(recordDate)
     })
-  } catch (err) {
-    console.warn(err)
-  }
 
-  try {
-    categories = await Category.find().lean().exec()
-
+    // Find all categories object
+    const categories = await Category.find().lean().exec()
+    // Iterate over categories and assign value to tempCategory key
     categories.forEach(el => {
       el.tempCategory = category
     })
-  } catch (err) {
-    console.warn(err)
-  }
 
-  try {
-    filteredRecords = await Record.find(filter)
+    // Find all filtered records
+    const filteredRecords = await Record.find(filter)
       .lean()
       .sort({ date: 'desc' })
       .exec()
-
     // Calculate total amount
+    let totalAmount = 0
     filteredRecords.forEach(record => {
       totalAmount += record.amount
     })
     // Format total amount
     totalAmount = new Intl.NumberFormat().format(totalAmount)
+
+    return res.render('index', {
+      records: filteredRecords,
+      totalAmount,
+      categoryList: categories,
+      selectDate: date,
+      dateSet,
+    })
   } catch (err) {
     console.warn(err)
   }
-
-  return res.render('index', {
-    records: filteredRecords,
-    totalAmount,
-    categoryList: categories,
-    selectDate: date,
-    dateSet,
-  })
 })
 
 // Export router
