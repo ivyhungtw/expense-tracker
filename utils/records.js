859f499e4e982ba5aadf27d451abfaa60f74e6d2
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-07-15 09:45:36 -0500

    feat: add records related methods

---
+++ utils/records.js
@@ -0,0 +1,68 @@
+const Record = require('../models/record')
+
+module.exports = {
+  async getAmountByMonth(userId) {
+    let amountByMonth = {}
+    const result = await Record.aggregate([
+      { $match: { userId } },
+      {
+        $group: {
+          _id: { year: { $year: '$date' }, month: { $month: '$date' } },
+          count: {
+            $sum: '$amount'
+          }
+        }
+      },
+      {
+        $sort: {
+          '_id.year': -1,
+          '_id.month': -1
+        }
+      }
+    ])
+
+    result.forEach(el => {
+      let date = Object.values(el._id).join('-')
+      amountByMonth[date] = el.count
+    })
+
+    return amountByMonth
+  },
+  async getAmountByCategory(filter) {
+    let amountByCategory = {}
+    const result = await Record.aggregate([
+      { $match: filter },
+      {
+        $group: {
+          _id: { category: '$categoryId' },
+          count: {
+            $sum: '$amount'
+          }
+        }
+      },
+      {
+        $sort: {
+          _id: 1
+        }
+      }
+    ])
+
+    result.forEach(el => {
+      let id = el._id.category
+      amountByCategory[id] = el.count
+    })
+
+    return amountByCategory
+  },
+  organizeCategoryData(categoryList, amountByCategory) {
+    const categoryObject = Object.assign(
+      ...categoryList.map(category => ({
+        [category.name]: amountByCategory[category._id] || 0
+      }))
+    )
+    categoryList.forEach(category => {
+      category.amount = categoryObject[category.name]
+    })
+    return categoryObject
+  }
+}
