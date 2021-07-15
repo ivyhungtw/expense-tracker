a2e10ebfd9ea8f3e98222bfc992287634b088300
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-07-15 17:19:30 -0500

    refactor: require record controller in home router

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,84 +2,10 @@
 const express = require('express')
 const router = express.Router()
 
-// Require models
-const Record = require('../../models/record')
-const Category = require('../../models/category')
-
-// Require other packages
-const moment = require('moment')
-
-const {
-  getAmountByMonth,
-  getAmountByCategory,
-  organizeCategoryData
-} = require('../../utils/records')
+const recordController = require('../../controllers/recordController')
 
 // Set up routes of homepage
-router.get('/', async (req, res) => {
-  try {
-    const userId = req.user._id
-    const filter = { userId }
-    const monthOfYearSet = new Set()
-    let totalAmount = 0
-    let totalExpense = 0
-    let totalRevenue = 0
-    const [records, categoryList, amountByMonth, amountByCategory] =
-      await Promise.all([
-        Record.find({ userId })
-          .populate('categoryId')
-          .lean()
-          .sort({ date: 'desc' })
-          .exec(),
-        Category.find().lean().exec(),
-        getAmountByMonth(filter),
-        getAmountByCategory(filter)
-      ])
-
-    const categoryObject = organizeCategoryData(categoryList, amountByCategory)
-
-    records.forEach(record => {
-      // Calculate total amount
-      if (record.type === 'expense') {
-        totalAmount -= record.amount
-        totalExpense -= record.amount
-      } else if (record.type === 'revenue') {
-        totalAmount += record.amount
-        totalRevenue += record.amount
-      }
-
-      const date = moment.utc(record.date)
-      // Reassign date format to render record list
-      record.date = date.format('YYYY-MM-DD')
-      // Store different months of years to render year-month filter
-      monthOfYearSet.add(date.format('YYYY-MM'))
-    })
-
-    // Save months of years to session for later use
-    req.session.monthOfYearSet = [...monthOfYearSet].join(' ')
-
-    // Format total amount
-    totalAmount = new Intl.NumberFormat().format(totalAmount)
-
-    return res.render('index', {
-      monthOfYearSet,
-      categoryList,
-      totalAmount,
-      records,
-      indexCSS: true,
-      categoryName: Object.keys(categoryObject),
-      categoryAmount: Object.values(categoryObject),
-      chart: true,
-      groupByMonth: Object.keys(amountByMonth),
-      amountByMonth: Object.values(amountByMonth),
-      home: true,
-      totalExpense,
-      totalRevenue
-    })
-  } catch (err) {
-    console.warn(err)
-  }
-})
+router.get('/', recordController.getRecords)
 
 // Export
 module.exports = router
