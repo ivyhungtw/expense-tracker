337c8fddd4a0cedebdf31787000cf91f202e1efd
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-07-28 23:25:45 -0500

    feat: show password

--- public/javascripts/utility.js
+++ public/javascripts/utility.js
@@ -3,4 +3,16 @@ const checkDelete = () => {
   return window.confirm('Are you sure to delete it?')
 }
 
-module.exports = checkDelete
+// Toggle password type
+const togglePassword = document.querySelector('#togglePassword')
+const password = document.querySelector('#password')
+
+togglePassword.addEventListener('click', function (e) {
+  // toggle the type attribute
+  const type =
+    password.getAttribute('type') === 'password' ? 'text' : 'password'
+  password.setAttribute('type', type)
+  // toggle the eye / eye slash icon
+  this.classList.toggle('fa-eye')
+  this.classList.toggle('fa-eye-slash')
+})
